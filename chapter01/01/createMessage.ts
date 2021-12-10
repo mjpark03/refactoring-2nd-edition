a8f85bbf3eb013d7d20d5ee4f341af09ee1bb79e
mjpark03 <mjpark03@gmail.com>
2021-12-10 12:34:29 +0900

    Add example file for 1.1 자, 시작해보자!

---
+++ chapter01/01/createMessage.ts
@@ -0,0 +1,37 @@
+import { IMessage } from '../types'
+import moment from 'moment-timezone'
+import 'moment/locale/ko'
+
+export default function createMessage (
+    type: string,
+    data: string,
+    userId: number,
+    channelId: string
+): IMessage {
+    let content = {}
+    const now = moment()
+    const displayDate = moment(now).tz('Asia/Seoul').format('LT')
+
+    switch(type) {
+        case 'text':
+          content = {
+            text: data
+          }
+          break
+        case 'qna':
+          content = {
+            qna: data
+          }
+          break
+    }
+
+    const message: IMessage = {
+      from: userId,
+      to: channelId,
+      contentType: type,
+      ...content,
+      displayDate
+    }
+    
+    return message
+}
