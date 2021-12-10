5052464eccc6d4fd6442f407994cf64e8e88f4f5
mjpark03 <mjpark03@gmail.com>
2021-12-10 17:36:59 +0900

    Apply extract function for 1.4 statement() 함수 쪼개기
    - statement() 함수 대신, 작성자 본인의 코드인 createMessage() 함수에 적용

---
+++ chapter01/04/01_extract_function_createMessage.ts
@@ -0,0 +1,42 @@
+import { IMessage } from '../data/types'
+import moment from 'moment-timezone'
+import 'moment/locale/ko'
+
+export default function createMessage (
+    type: string,
+    data: string,
+    userId: number,
+    channelId: string,
+    now: Date
+): IMessage {
+    const content = contentFor(type) // 추출한 함수 사용
+    const displayDate = moment(now).tz('Asia/Seoul').format('LT')
+    const message: IMessage = {
+      from: userId,
+      to: channelId,
+      contentType: type,
+      ...content,
+      displayDate
+    }
+    
+    return message
+
+    function contentFor(type: string) { // 기존 switch문을 함수로 추출
+      let result = {}
+      switch(type) {
+        case 'text':
+          result = {
+            text: data
+          }
+          break
+        case 'qna':
+          result = {
+            qna: data
+          }
+          break
+        default:
+          throw new Error(`지원하지 않는 유형: ${type}`)
+      }
+      return result
+    }
+}
