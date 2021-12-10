0fd76698ea72db08ac7298cca640bc9001bbaa9f
mjpark03 <mjpark03@gmail.com>
2021-12-10 17:21:15 +0900

    Add test file for 1.3 리팩터링의 첫 단계

---
+++ chapter01/03/createMessage.ts
@@ -0,0 +1,39 @@
+import { IMessage } from '../data/types'
+import moment from 'moment-timezone'
+import 'moment/locale/ko'
+
+export default function createMessage (
+    type: string,
+    data: string,
+    userId: number,
+    channelId: string,
+    now: Date
+): IMessage {
+    let content = {}
+    const displayDate = moment(now).tz('Asia/Seoul').format('LT')
+
+    switch(type) {
+        case 'text':
+          content = {
+            text: data
+          }
+          break
+        case 'qna':
+          content = {
+            qna: data
+          }
+          break
+        default:
+          throw new Error(`지원하지 않는 유형: ${type}`)
+    }
+
+    const message: IMessage = {
+      from: userId,
+      to: channelId,
+      contentType: type,
+      ...content,
+      displayDate
+    }
+    
+    return message
+}
